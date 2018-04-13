ca6aca57dc535d5e56f6ed82e82cfa91ee0d9b06
thefirsthuman <Scipio172316>
2018-04-13 13:55:39 -0700

    Initial Commit

---
+++ portfolio/js/main.js
@@ -0,0 +1,9 @@
+// var menuOpen = false;
+
+$('#menu-toggle').click(function () {
+    $('#sidebar').toggleClass("visible");
+});
+
+// $('.segment').dimmer('hide');
+
+$('.fade_up_intro').fadeIn();
